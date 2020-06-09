afb18e6859c5e13cc448043e77109c9a820ba275
kotoku-code <kennedy.kotoku@gmail.com>
2020-06-09 14:32:52 +0200

    added link for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_092643) do
+ActiveRecord::Schema.define(version: 2020_06_09_105251) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author_name"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
 
   create_table "links", force: :cascade do |t|
     t.string "title"
